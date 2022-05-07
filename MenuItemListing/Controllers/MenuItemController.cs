f065272353c1ee84f03ea00f6a4aa4f82ee5e8e3
unknown <Surajit Nandy@DESKTOP-A64KESB.cts.com>
2022-05-07 14:53:08 +0530

    Initial commit

---
+++ MenuItemListing/Controllers/MenuItemController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MenuItemListing.Models;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MenuItemListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MenuItemController : ControllerBase
+    {
+        private List<MenuItem> _menuItems = new List<MenuItem>
+        {
+            new MenuItem
+            {
+                Id = 1,
+                Name = "Pizza",
+                FreeDelivery = true,
+                Price = 200,
+                DateOfLaunch = DateTime.Now,
+                Active = true
+            }, 
+            new MenuItem
+            {
+                Id = 2,
+                Name = "Biriyani",
+                FreeDelivery = false,
+                Price = 300,
+                DateOfLaunch = DateTime.Now,
+                Active = true
+            },
+            new MenuItem
+            {
+                Id = 3,
+                Name = "Fried Rice",
+                FreeDelivery = false,
+                Price = 150,
+                DateOfLaunch = DateTime.Now,
+                Active = true
+            },
+            new MenuItem
+            {
+                Id = 4,
+                Name = "Chicken",
+                FreeDelivery = true,
+                Price = 200,
+                DateOfLaunch = DateTime.Now,
+                Active = false
+            }
+        };
+
+        // GET: api/<MenuItemController>
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            return _menuItems;
+        }
+
+        // GET api/<MenuItemController>/5
+        [HttpGet("{id}")]
+        public MenuItem? Get(int id)
+        {
+            return _menuItems.Find(menuItem => menuItem.Id == id);
+        }
+       
+    }
+}
---
+++ MenuItemListing/Models/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MenuItemListing.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name="Free Delivery")]
+        public bool FreeDelivery { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfLaunch { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
